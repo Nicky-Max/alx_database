4561184d55eee5436e171133c40bfd6cf2e9be91
Nicholas Kipruto <nicholaskipruto112@gmail.com>
2023-08-18 22:25:02 +0300

    changes

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -2,18 +2,14 @@
 
 
 
--- Check if the database name is provided as an argument
-IF [ $# -ne 1 ]; THEN
-    echo "Usage: $0 <database_name>"
-    exit 1
-FI
 
--- Capture the provided database name
-database_name=$1
+
+-- switch to the database whose name is passed as an argument
+USE $1
 
 
 
--- Query to list all tables in the specified database
+-- lists all tables in the specified database
 SHOW TABLES FROM $database_name;
 
 
