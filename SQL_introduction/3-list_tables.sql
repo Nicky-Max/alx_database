75f47598c74f7639959526ea40b461da2d17f639
Nicholas Kipruto <nicholaskipruto112@gmail.com>
2023-08-19 11:58:09 +0300

    task3

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,16 +1,7 @@
--- listing of tables in database
-
-
-
-
-
--- switch to the database whose name is passed as an argument
-USE $1
-
 
 
 -- lists all tables in the specified database
-SHOW TABLES FROM $database_name;
+SHOW TABLES;
 
 
 
