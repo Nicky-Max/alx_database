b82f8610a578221dc42aab6a9f128b2f44796620
Nicholas Kipruto <nicholaskipruto112@gmail.com>
2023-08-19 14:40:56 +0300

    task2 updated

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -2,10 +2,10 @@
 
 CREATE SCHEMA IF NOT EXISTS hbtn_0d_2;
 -- create user 
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
 
 -- create a password for the user
-SET PASSWORD FOR 'user_0d_2'@'localhost' = "user_0d_2_pwd"
+SET PASSWORD FOR 'user_0d_2'@'localhost' = "user_0d_2_pwd";
 -- grant SELECT privilege to the user
 
-GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost'
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
