8b8cf5940e4c0e562ef6e7654b98fa5c59f46d2c
Nicholas Kipruto <nicholaskipruto112@gmail.com>
2023-08-19 13:34:50 +0300

    TASK 0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
 -- grants all privilege to users 
 
-GRANT ALL PRIVILEGES ON *.* TO "user_0d_1"@"localhost";
-GRANT ALL PRIVILEGES ON *.* TO "user_0d_2"@"localhost";
+GRANT ALL PRIVILEGES ON *.* TO IF EXISTS "user_0d_1"@"localhost";
+GRANT ALL PRIVILEGES ON *.* TO IF EXISTS "user_0d_2"@"localhost";
 
