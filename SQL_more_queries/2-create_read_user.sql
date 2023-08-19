a1e17fa64e13d41081da565bd3f39e34d3ae03a9
Nicholas Kipruto <nicholaskipruto112@gmail.com>
2023-08-19 14:30:16 +0300

    chages

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -8,4 +8,4 @@ CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
 SET PASSWORD FOR 'user_0d_2'@'localhost' = "user_0d_2_pwd"
 -- grant SELECT privilege to the user
 
-GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost'
+GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost'
