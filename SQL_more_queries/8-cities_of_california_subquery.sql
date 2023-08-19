8035cecc54608c09f7806d20f7b72e686743a54e
Nicholas Kipruto <nicholaskipruto112@gmail.com>
2023-08-19 22:32:45 +0300

    changes

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,14 +1,13 @@
 --  lists all the cities of California that can be found in the database hbtn_0d_usa
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
+
+
+-- create table
+CREATE TABLE IF NOT EXISTS states(id int UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS cities(id int UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+                                    state_id int NOT NULL, FOREIGN KEY(state_id) REFERENCES states(id),
+                                     name VARCHAR(256) NOT NULL);
 -- inserting values to the states table
-INSERT INTO states (id, name) VALUES (1, "California"),(2, "Arizona"),(3, "Texas"),(4, "Utah");
--- inserting values to the cities table
-INSERT INTO cities (id, state_id, name) VALUES (1, 1, "San Francisco"),
-                                             (2, 1, "San Jose"),
-                                             (4, 2, "Page"),
-                                             (6, 3, "Paris"),
-                                             (7, 3, "Houston"),
-                                             (8, 3, "Dallas");
+INSERT INTO states (id, name) VALUES (1, "California");
+
 
 SELECT id, name FROM cities WHERE state_id IN (SELECT id FROM states WHERE name = 'California') ORDER BY id;                                          
