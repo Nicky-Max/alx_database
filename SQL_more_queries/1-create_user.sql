6b0ca7052cdd660bfff269fefa64a2d5135fd883
Nicholas Kipruto <nicholaskipruto112@gmail.com>
2023-08-19 14:05:19 +0300

    changes

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- creates user 
-CREATE USER IF NOT EXIST 'user_0d_1'@'localhost'
+CREATE USER IF NOT EXIST 'user_0d_1'@'localhost';
 
 -- sets password for the user
-SET PASSWORD FOR 'user_0d_1'@'localhost' = "user_0d_1_pwd"
+SET PASSWORD FOR 'user_0d_1'@'localhost' = "user_0d_1_pwd";
